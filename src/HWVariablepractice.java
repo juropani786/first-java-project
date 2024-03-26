68468ea4e9303ca2971fe5295e5d983b31db03c7
juropani786 <163758390+juropani786@users.noreply.github.com>
2024-03-25 22:02:41 -0400

    Update HWVariablepractice.java

--- src/HWVariablepractice.java
+++ src/HWVariablepractice.java
@@ -7,11 +7,13 @@ public class HWVariablepractice {
 		float passedPercentage = 78.5f;
 		int passedWithAGrade = 20;
 		boolean isgoodrateofpresence = false;
+		String str = "Ramesh";
 		System.out.println(" Total Number of the people" +peopleNum);
 		System.out.println(" Total attendees" +presentNum );
 		System.out.println(" People who got through"+passedPercentage);
 		System.out.println("People who got A Grade in exam" +passedWithAGrade);
 		System.out.println(" only few people were present" +isgoodrateofpresence);
+		system.out println ("String is a non primitive data type"+str);
 		
 	}
 }
